45cb43c81f1240c9b184e86fc8d46068a2e267e6
Tyler Harris <tylerdavidharris@gmail.com>
2017-08-30 20:38:05 -0600

    added react-ga for Google Analytics, and fixed title

--- components/header/header.js
+++ components/header/header.js
@@ -15,6 +15,7 @@ export default class extends React.Component {
       <header>
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <title>Vance Harris, M.D.</title>
           <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css' />
           <link rel='stylesheet' type='text/css' href='static/lib/bootstrap-theme-toolkit/toolkit-startup.min.css' />
           <link rel='stylesheet' type='text/css' href='static/css/global.css' />
--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,11 +2,21 @@ import React from 'react'
 import Header from '../header/header'
 import Footer from '../footer/footer'
 
-export default class extends React.Component {
+import { initGA, logPageView } from '../../utils/analytics'
+
+export default class Layout extends React.Component {
   static propTypes () {
     return {
       children: React.PropTypes.object.isRequired
     }
+  }
+
+  componentDidMount () {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
   }
 
   render () {
