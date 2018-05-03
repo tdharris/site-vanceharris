e9f55edffd1329a7bc5b4c8e6ef2fd6aa5eecce2
Tyler Harris <tylerdavidharris@gmail.com>
2018-05-03 14:19:12 -0600

    update & fix bootstrap to specific version

--- components/header/header.js
+++ components/header/header.js
@@ -16,7 +16,8 @@ export default class extends React.Component {
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <title>Vance Harris, M.D.</title>
-          <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css' />
+          <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' 
+            integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossOrigin='anonymous' />
           <link rel='stylesheet' type='text/css' href='static/lib/bootstrap-theme-toolkit/toolkit-startup.min.css' />
           <link rel='stylesheet' type='text/css' href='static/css/global.css' />
         </Head>
